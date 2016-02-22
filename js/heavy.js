aacdfc5f7c357821a799deee18d960a60f2064ed
Kieran Topping <kieran@unbound.co.uk>
2016-02-22 21:40:24 +0000

    x

--- js/heavy.js
+++ js/heavy.js
@@ -1,6 +1,6 @@
 $(function() {
   for (var i=0; i<100 ; i++) {
-    $("body").append("(" + ii + "), ");
+    $("body").append("(" + i + "), ");
     (function (ii) {
         setTimeout(function () { $("body").append("" + ii + ", ");}, 500 + ii);
     })(i);
