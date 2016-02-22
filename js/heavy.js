252eee22ce640a130d3c767eed7daebadadf94df
Kieran Topping <kieran@unbound.co.uk>
2016-02-22 21:32:06 +0000

    x

--- js/heavy.js
+++ js/heavy.js
@@ -1,5 +1,7 @@
-for (var i=0; i<100 ; i++) {
-  (function (ii) {
-      setTimeout(function () { $("body").append("" + ii + ", ");}, 500 + ii);
-  })(i);
-}
+$(function() {
+  for (var i=0; i<100 ; i++) {
+    (function (ii) {
+        setTimeout(function () { $("body").append("" + ii + ", ");}, 500 + ii);
+    })(i);
+  }
+});
