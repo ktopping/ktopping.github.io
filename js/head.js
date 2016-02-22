35882614d8029599976f6703cefd50453150f3d1
Kieran Topping <kieran@unbound.co.uk>
2016-02-22 16:42:01 +0000

    x

--- js/head.js
+++ js/head.js
@@ -1,13 +1,17 @@
-			WebFontConfig = {
-			google: { families: [ 'Source+Sans+Pro:300,400,600,300italic,400italic,600italic:latin' ] }
-			};
-			(function() {
-				var wf = document.createElement('script');
-				wf.src = ('https:' == document.location.protocol ? 'https' : 'https') +
-					'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-				wf.type = 'text/javascript';
-				wf.async = 'true';
-				var s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(wf, s);
-			})();
+WebFontConfig = {
+google: { families: [ 'Source+Sans+Pro:300,400,600,300italic,400italic,600italic:latin' ] }
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'https') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
 
+
+for (var i=0; i<1000 ; i++) {
+  console.log(i);
+}
